e89c15b969c53334d493b8a5a44894c820c2b3ba
witchkaa <victoriasabadan1@gmail.com>
2024-10-23 21:39:14 +0300

    Init (lab1)

---
+++ src/test/java/ua/kpi/ist/springlab1/SpringLab1ApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.kpi.ist.springlab1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringLab1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
